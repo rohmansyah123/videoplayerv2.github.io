2f0ac0bcc62d7f81caad13459b123deab271a878
ROHMANSYAH <40875901+rohmansyah123@users.noreply.github.com>
2025-06-08 22:15:55 +0700

    Create adsteraparallax.js

---
+++ data/assets/adsteraparallax.js
@@ -0,0 +1,2 @@
+<script async="async" data-cfasync="false" src="//pl26583030.profitableratecpm.com/2c979ea6eea470e28aecac661089d1a9/invoke.js"></script>
+<div id="container-2c979ea6eea470e28aecac661089d1a9"></div>
